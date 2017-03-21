1f1926249a3a7100c837da665d2d14da17ee3407
Maxim Topciu <maximtop@gmail.com>
2017-03-21 13:36:19 +0300

    adds rollerbar before deploying to heroku

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# project-lvl4-s108
+# nodejs-package
+
+[![Code Climate](https://codeclimate.com/github/hexlet-boilerplates/nodejs-package/badges/gpa.svg)](https://codeclimate.com/github/hexlet-boilerplates/nodejs-package)
+[![Issue Count](https://codeclimate.com/github/hexlet-boilerplates/nodejs-package/badges/issue_count.svg)](https://codeclimate.com/github/hexlet-boilerplates/nodejs-package)
+[![Build Status](https://travis-ci.org/hexlet-boilerplates/nodejs-package.svg?branch=master)](https://travis-ci.org/hexlet-boilerplates/nodejs-package)
+
+## Setup
+
+```
+make install
+```
+
+Created postgresql-shallow-45831 as DATABASE_URL
---
+++ src/index.js
@@ -0,0 +1,71 @@
+// @flow
+
+import 'babel-polyfill';
+
+import path from 'path';
+import Koa from 'koa';
+import Pug from 'koa-pug';
+import Router from 'koa-router';
+import koaLogger from 'koa-logger';
+import serve from 'koa-static';
+import middleware from 'koa-webpack';
+import bodyParser from 'koa-bodyparser';
+import session from 'koa-generic-session';
+import flash from 'koa-flash-simple';
+import _ from 'lodash';
+import methodOverride from 'koa-methodoverride';
+import rollbar from 'rollbar';
+
+import getWebpackConfig from '../webpack.config.babel';
+import addRoutes from './controllers';
+import container from './container';
+
+export default () => {
+  const app = new Koa();
+  rollbar.init('6ccd6ab658dc4c7e884fac2b00fc5b45');// TODO Find out better place to keep this token
+
+  app.keys = ['some secret hurr'];
+  app.use(session(app));
+  app.use(flash());
+  app.use(async (ctx, next) => {
+    ctx.state = {
+      flash: ctx.flash,
+      isSignedIn: () => ctx.session.userId !== undefined,
+    };
+    await next();
+  });
+  app.use(bodyParser());
+  app.use(methodOverride((req) => {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      return req.body._method;
+    }
+  }));
+  app.use(serve(path.join(__dirname, '..', 'public')));
+  app.use(middleware({
+    config: getWebpackConfig(),
+  }));
+
+  app.use(koaLogger());
+  const router = new Router();
+  addRoutes(router, container);
+  app.use(router.allowedMethods());
+  app.use(router.routes());
+
+  const pug = new Pug({
+    viewPath: path.join(__dirname, 'views'),
+    debug: true,
+    pretty: true,
+    compileDebug: true,
+    locals: [],
+    basedir: path.join(__dirname, 'views'),
+    helperPath: [
+      { _ },
+      { urlFor: (...args) => router.url(...args) },
+    ],
+  });
+  pug.use(app);
+  app.on('error', err => {
+    rollbar.handleError(err);
+  });
+  return app;
+};
