49b2e274fbc62e18eb4d1bd19d3cbd450d64b0cd
Maxim Topciu <maximtop@gmail.com>
2017-03-21 16:16:32 +0300

    Add postcss loader + autopreficser

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -25,7 +25,7 @@ export default () => ({
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
     ],
   },
