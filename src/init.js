c1606560ac653f3f6341ae2b09439fe633d75709
Maxim Topciu <maximtop@gmail.com>
2017-04-17 16:34:35 +0300

    sf

--- src/init.js
+++ src/init.js
@@ -11,8 +11,8 @@ export default async () => {
   //   }
   // }));
   await models.User.sync({ force: true });
-  await models.Task.sync({ force: true });
   await models.TaskStatus.sync({ force: true });
+  await models.Task.sync({ force: true });
   await models.Tag.sync({ force: true });
   await models.TaskTag.sync({ force: true });
   await models.TaskStatus.bulkCreate([
